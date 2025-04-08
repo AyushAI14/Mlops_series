5d4db080f1229e09d896a56a08fa06453fab9bfe
AyushAI14 <ayushaiml14@gmail.com>
2025-04-08 10:41:30 +0530

    made a basic layout the of project

--- Day_2_oops_python/Social_Oops_project.py
+++ Day_2_oops_python/Social_Oops_project.py
@@ -1,2 +1,28 @@
 print("Oops mini project : ChatBook")
 
+
+class ChatBook:
+    def __init__(self):
+       self.name = ''
+       self.password = ''
+       self.login = False
+       self.menu()
+
+    def menu(self):
+        user_input=input("""
+                        Hello to the welcome page of ChatBook
+                        Press 1 to signin
+                        Press 2 to signup
+                        Press 3 to login
+                        Hit any key to exit
+                         """)
+        if user_input == '1':
+            print("you pressed 1")
+        elif user_input == '2':
+            pass
+        elif user_input == '3':
+            pass
+        else:
+            exit()
+
+obj=ChatBook()
