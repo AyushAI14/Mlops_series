a3bf304f812bdc45c788a1ac7729f798472a93ab
AyushAI14 <ayushaiml14@gmail.com>
2025-04-09 13:55:19 +0530

    DVC intiated with data version2 fully done

--- Day_3_DVC/index.py
+++ Day_3_DVC/index.py
@@ -3,13 +3,20 @@ print("learning DVC")
 import pandas as pd
 import os
 
+
 my_data = {
         'name':['ayush','piyush'],
         'age':[19,16],
         'occu':['ml engineer','student']
         }
 
+
+#adding data 
+add_data = {'name':'anj','age':14,'occu':'student'}
+
 data = pd.DataFrame(my_data)
+data.loc[len(data.index)] = add_data 
+
 print(data)
 
 mydir = 'DataDir'
